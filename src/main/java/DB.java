d6cae07378e806655583d4ea14582e67cdd6a540
JOSEPH CHEGE <josephchege08@gmail.com>
2018-01-05 18:30:30 +0300

    Modified JB.java

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,27 +1,28 @@
-import org.sql2o.*;
-import java.net.URI;
+import org.sql2o.*; 
+import java.net.URI; 
 import java.net.URISyntaxException;
-
-public class DB {
-    private static URI dbUri;
+import org.slf4j.Logger;                       
+import org.slf4j.LoggerFactory;
+public class DB { 
+    private static URI dbUri; 
     public static Sql2o sql2o;
-
+    Logger logger = LoggerFactory.getLogger(DB.class);
     static {
-
+    
         try {
             if (System.getenv("DATABASE_URL") == null) {
                 dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
             } else {
                 dbUri = new URI(System.getenv("DATABASE_URL"));
             }
-
             int port = dbUri.getPort();
             String host = dbUri.getHost();
             String path = dbUri.getPath();
             String username = (dbUri.getUserInfo() == null) ? "joseph" : dbUri.getUserInfo().split(":")[0];
             String password = (dbUri.getUserInfo() == null) ? "chege" : dbUri.getUserInfo().split(":")[1];
-            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
-        } catch (URISyntaxException e ) {
+sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) { 
+            logger.error("Unable to connect to database.");     
         }
     }
 }
