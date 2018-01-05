82e5f4d550d02d5045eb3a462e54e077fdc6311f
JOSEPH CHEGE <josephchege08@gmail.com>
2018-01-05 18:23:01 +0300

    Modified App.javaProcessBuilder process = new ProcessBuilder();     Integer port;
        // This tells our app that if Heroku sets a port for us, we need to use that port.
        // Otherwise, if they do not, continue using port 4567.
    
        if (process.environment().get("PORT") != null) {
            port = Integer.parseInt(process.environment().get("PORT"));
        } else {
            port = 4567;
        }
    
        setPort(port);y

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,15 +9,18 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-     ProcessBuilder process = new ProcessBuilder(); 
+    ProcessBuilder process = new ProcessBuilder();
     Integer port;
+
     // This tells our app that if Heroku sets a port for us, we need to use that port.
     // Otherwise, if they do not, continue using port 4567.
+
     if (process.environment().get("PORT") != null) {
         port = Integer.parseInt(process.environment().get("PORT"));
     } else {
         port = 4567;
     }
+
     setPort(port);
 
     // Get Routes
